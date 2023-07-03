afc9f49d1352419f2214b46782bb3231fdebf665
s0ham075 <sohamsawant075@gmail.com>
2023-07-03 20:44:45 +0530

    models task completed

--- registration/models.py
+++ registration/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class User(models.Model):
     pass
 
-class Tags(models.Model):
-    tag_name = models.CharField()
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=200)
 
 
 class Event(models.Model):
@@ -17,19 +17,19 @@ class Event(models.Model):
     event_desc=models.TextField()
     event_banner=models.FileField()
     event_thumbnail=models.FileField()
-    tags =models.ManyToManyField(Tags)
+    tags =models.ManyToManyField(Tag)
     location=models.TextField()
-    status = models.CharField(choices=Options)
+    status = models.CharField(max_length=200,choices=Options)
     event_created_on = models.DateField()
     virtual_meet_instructions=models.TextField()
     virtual_meet_link= models.URLField()
-    speaker = models.CharField(null=True)
+    speaker = models.CharField(max_length=200,null=True)
 
 class Schedule(models.Model):
-    event = models.ForeignKey(Event)
+    event = models.ForeignKey(Event,on_delete=models.SET_NULL,null=True)
     no_of_days= models.BigIntegerField()
     start_time= models.TimeField()
     end_time= models.TimeField()
     event_schedule = models.JSONField(default=dict(day_title="", start_time="", end_time=""))
-    
+
 
